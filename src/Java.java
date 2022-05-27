c19f3dd6883100b7657b4e93006c88d653c0668f
quicknightbird <eliasriv73@gmail.com>
2022-05-27 08:47:25 +0300

    change solution one more

--- src/Java.java
+++ src/Java.java
@@ -1,5 +1,7 @@
+import Algorithms.IntegerToRoman;
 import Algorithms.RomanToInteger;
 import Algorithms.HappyNumber;
+import Algorithms.RomanToInteger;
 //Ⅰ（1） Ⅴ（5） Ⅹ（10） Ⅼ（50） Ⅽ（100） Ⅾ（500）and Ⅿ（1000）
 public class Java {
     public static void main(String[] args) {
@@ -8,7 +10,11 @@ public class Java {
 //        RomanToInteger clas = new RomanToInteger();
 //        System.out.println(clas.romanToInt("MCMXCIV"));
 
-        HappyNumber number = new HappyNumber();
-        System.out.println(number.isHappy(54));
+//        HappyNumber number = new HappyNumber();
+//        System.out.println(number.isHappy(54));
+
+        IntegerToRoman RomNum = new IntegerToRoman();
+        System.out.println(RomNum.intToRoman(1994));
+
     }
 }
